f4595dd2b5229d519906a9b2ffbfa3f475c89f00
iAm9001 <34911156+iAm9001@users.noreply.github.com>
2022-11-09 00:08:16 -0500

    Add Documentation

--- ConfigsClassXmlDocToJsonComments/ConfigsClassXmlDocToJsonComments/ConfigsClassXmlDocToJsonComments.cs
+++ ConfigsClassXmlDocToJsonComments/ConfigsClassXmlDocToJsonComments/ConfigsClassXmlDocToJsonComments.cs
@@ -1,5 +1,11 @@
 ﻿namespace ConfigsClassXmlDocToJsonComments;
 
-public class ConfigsClassXmlDocToJsonComments
+/// <summary>
+/// The <see cref="ConfigsClassXmlDocToJsonComments"/> class is a simple
+/// extension methods class that allows you to serialize a .NET class (along
+/// with it's XMLDOC source comments, including those on it's properties)
+/// to JSONC (JSON With Comments).
+/// </summary>
+public static class ConfigsClassXmlDocToJsonComments
 {
 }
